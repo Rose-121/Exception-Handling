ebaee16a0d0010b11809b201fc2c72a7858485b7
Rose <mehrabkhanrose10@gmail.com>
2024-09-11 18:52:59 +0600

    rose

---
+++ Multiple exceptions.py
@@ -0,0 +1,24 @@
+print("\033c")
+try:
+    num1 = int(input("Enter a number:"))
+    num2 = int(input("Enter a number:"))
+    
+    result = num1/num2
+    
+    print("Result is :" , result)
+    print("Result is :" , result1)
+    
+except ZeroDivisionError:
+    print("Division by zero is not allowed")
+    
+except ValueError:
+    print("Please Enter numerical value")
+    
+except NameError as ex :
+    print("The exception is",ex)
+    
+finally:
+    print("I will execute no matter what happens")
+    z = 15*15
+    print(z)
+              
